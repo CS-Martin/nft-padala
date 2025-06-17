1ef4f0024b7510ea0b494b2501aec391521d8410
Jeck <zrkeimperial@gmail.com>
2025-06-17 22:15:13 +0800

    feat: add Logo component for branding display

---
+++ src/components/block/logo.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Logo() {
+    return (
+        <div className='flex items-center gap-3'>
+            <div className='h-[25px] w-[25px] bg-primary rounded'></div>
+            <span>NFTPadala</span>
+        </div>
+    );
+}
