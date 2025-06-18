9679bb8a5d2f089cb91d606a6268b6c17a102898
Jeck <zrkeimperial@gmail.com>
2025-06-18 08:53:46 +0800

    feat(wallet): enhance WalletConnection component with avatar and updated button styles

--- src/components/custom/wallet-connection.tsx
+++ src/components/custom/wallet-connection.tsx
@@ -4,7 +4,9 @@ import { WalletStatus } from '@/types/wallet-status.type';
 import { Button } from '../ui/button';
 import NeumorphButton from '../ui/neumorph-button';
 import { TextureButton } from '../ui/texture-button';
-import { WalletMinimal } from 'lucide-react';
+import { ChevronDown, LogOut, WalletMinimal } from 'lucide-react';
+import { IoIosWallet } from 'react-icons/io';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
 interface WalletConnectionProps {
     walletStatus: WalletStatus;
@@ -17,20 +19,41 @@ export const WalletConnection = ({ walletStatus, connectWallet, disconnectWallet
     return (
         <div>
             {walletStatus.isConnected ? (
-                <Button
-                    onClick={disconnectWallet}
-                    disabled={isLoading}
-                    className='bg-red-500 cursor-pointer hover:bg-red-700 text-white font-bold py-2 px-4 rounded'>
-                    Disconnect Wallet ({walletStatus.address?.slice(0, 6)}...
-                    {walletStatus.address?.slice(-4)})
-                </Button>
+                <>
+                    <div className='flex items-center gap-3'>
+                        <Button
+                            onClick={disconnectWallet}
+                            disabled={isLoading}
+                            className='cursor-pointer'
+                            variant={'ghost'}>
+                            <Avatar className='w-[25px] h-[25px]'>
+                                <AvatarImage src='https://github.com/shadcn.png' />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                            <span>
+                                {walletStatus.address?.slice(0, 6)}...
+                                {walletStatus.address?.slice(-4)}
+                            </span>
+                            <LogOut size={10} />
+                        </Button>
+                    </div>
+
+                    {/* <Button
+                        onClick={disconnectWallet}
+                        disabled={isLoading}
+                        className='bg-red-500 cursor-pointer hover:bg-red-700 text-white font-bold py-2 px-4 rounded'>
+                        Disconnect Wallet ({walletStatus.address?.slice(0, 6)}...
+                        {walletStatus.address?.slice(-4)})
+                    </Button> */}
+                </>
             ) : (
                 <>
                     <Button
                         onClick={connectWallet}
                         disabled={isLoading}
-                        className='cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
-                        <WalletMinimal size={12} /> Connect Wallet
+                        variant={'ghost'}
+                        className='cursor-pointer'>
+                        <IoIosWallet /> Connect Wallet
                     </Button>
                 </>
             )}
