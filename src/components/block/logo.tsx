dc9ee117efa61e858985f3e6e651c4b8e3026469
Jeck <zrkeimperial@gmail.com>
2025-06-18 08:53:34 +0800

    feat(logo): add text size styling to NFTPadala in Logo component

--- src/components/block/logo.tsx
+++ src/components/block/logo.tsx
@@ -4,7 +4,7 @@ export default function Logo() {
     return (
         <div className='flex items-center gap-3'>
             <div className='h-[25px] w-[25px] bg-primary rounded'></div>
-            <span>NFTPadala</span>
+            <span className='text-sm'>NFTPadala</span>
         </div>
     );
 }
