d6a25563318dec9bce26cbe168071636949db1ae
Jeck <zrkeimperial@gmail.com>
2025-06-18 22:08:31 +0800

    refactor(nav): simplify NavBar by removing unnecessary wrapper around WalletButton

--- src/components/block/nav.tsx
+++ src/components/block/nav.tsx
@@ -5,26 +5,13 @@ import Logo from './logo';
 import { WalletConnection } from '../custom/wallet-connection';
 import { injected, useAccount, useConnect, useDisconnect } from 'wagmi';
 import { useWalletStore } from '@/stores/wallet.store';
-import { Github } from 'lucide-react';
-import { Button } from '../ui/button';
-import Link from 'next/link';
 
 export default function NavBar() {
     return (
         <nav className='h-[60px] w-full border-b flex items-center justify-between px-[20px] lg:px-[100px] 2xl:px-[200px]'>
             <Logo />
 
-            <div className='flex items-center gap-2'>
-                <WalletButton />
-                <Button
-                    size='icon'
-                    variant={'outline'}
-                    asChild>
-                    <Link href=''>
-                        <Github />
-                    </Link>
-                </Button>
-            </div>
+            <WalletButton />
         </nav>
     );
 }
