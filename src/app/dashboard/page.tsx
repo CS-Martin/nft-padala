f4155041d49f8a51cea520f1f151cce2ce1a65d8
Jeck <zrkeimperial@gmail.com>
2025-06-18 22:08:44 +0800

    feat(dashboard): restructure DashboardPage layout for improved readability and include Footer component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,23 @@
 import { Breadcrumbs } from '@/components/custom/breadcrumbs';
 import TransactionDataTable from './_components/recent-transactions/transaction-data-table';
+import { Footer } from '@/components/block/footer';
 
 export default function DashboardPage() {
     return (
-        <div>
-            <Breadcrumbs
-                items={[
-                    { label: 'Home', href: '/' },
-                    { label: 'Dashboard', href: '/dashboard' },
-                ]}
-            />
-            <TransactionDataTable />
-        </div>
+        <>
+            <main className='px-[20px] lg:px-[100px] 2xl:px-[200px]'>
+                <section>
+                    <Breadcrumbs
+                        items={[
+                            { label: 'Home', href: '/' },
+                            { label: 'Dashboard', href: '/dashboard' },
+                        ]}
+                    />
+                    <TransactionDataTable />
+                </section>
+            </main>
+
+            <Footer />
+        </>
     );
 }
